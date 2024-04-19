cd23eab8c07aefb2dc2e0437165af79f48e805d9
Jesus Janeiro Montes <forek4st@Jesuss-MacBook-Pro.local>
2024-04-18 20:10:38 -0600

    se crea server con express y se crea register y login con mongoose para mongodb

---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['ADMIN', 'COSTUMER'],
+    default: 'COSTUMER'
+  }
+})
+
+export default mongoose.model('User', userSchema)
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { connect } from "./config.js";
+import authRoutes from "./routes/authRoutes.js";
+
+const api = express();
+const PORT = 8000;
+connect();
+
+api.use(express.json());
+api.use("/auth", authRoutes);
+
+api.listen(PORT, () => {
+  console.log(`API running on ${PORT}`);
+});
+
+api.use((error, req, res, next) => {
+  if (error instanceof SyntaxError && "body" in error) {
+    res.status(400).json({
+      message: "Error: Invalid JSON format",
+      error: error.message,
+    });
+  } else {
+    res.status(500).json({
+      message: "Error: Internal server error",
+      error: error.message,
+    });
+  }
+});
