a42784f240dfb1932695a5dabca65622e94f4272
Jesus Janeiro Montes <forek4st@Jesuss-MacBook-Pro.local>
2024-04-19 20:07:01 -0600

    se anade cors al server

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import { connect } from "./config.js";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 
 const api = express();
+
 const PORT = 8000;
 connect();
 
+api.use(cors());
 api.use(express.json());
 api.use("/auth", authRoutes);
-api.use('/products', productRoutes)
+api.use("/products", productRoutes);
 
 api.listen(PORT, () => {
   console.log(`API running on ${PORT}`);
